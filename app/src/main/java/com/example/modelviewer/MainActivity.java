d756441357388db9aa4e1691ab02446ad0acedbc
duylehai <duylehai@github.com>
2022-12-25 20:04:14 +0700

    update ARViewer

---
+++ app/src/main/java/com/example/modelviewer/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.modelviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("creation","okay");
+        Intent intent = new Intent(this, ARView.class);
+        intent.setData(Uri.parse("https://storage.googleapis.com/ar-answers-in-search-models/static/Tiger/model.glb"));
+        startActivity(intent);
+    }
+}
